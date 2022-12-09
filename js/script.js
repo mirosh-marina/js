906d53e9f63c755aeda33362185fefd338852e71
Marina <example@gmail.com>
2022-12-09 23:29:09 +0400

    new file

---
+++ js/script.js
@@ -0,0 +1,128 @@
+"use strict"; 
+
+console.log('arr' + ' - object');
+console.log(4 + +'5');
+
+let incr = 10, 
+    decr = 10;
+
+// ++incr; 
+// decr--; 
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2 + 2 * 2 != 6); присваивание
+console.log(2*4 == '8' или 8); булиновое значение
+console.log(2*4 === 8); строгое равенство
+
+&& - оператор и работает только если все значения правдивы
+|| - оператор или хотя бы одно значение правда
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && !isClose);
+
+! оператор отрицание
+
+function multiply(a, b){
+    a * b
+  }
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'ivan';
+
+// alert(`привет, ${user}`);
+
+// const storeName = 'Magnit';
+// const storeDescription = {
+//         budget: 10000,
+//         employees: ['Ivan', 'Daria', 'Marina'],
+//         'products': {
+//             'tea': 10,
+//            ' coffee': 20
+//         },
+//             open: true, 
+//         };
+
+//   alert('hello');
+
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+
+// const answer = +prompt('вам есть 18?', '18');
+// console.log(answer + 5); 
+
+// const answers = [];
+
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваша фамилия?', '');
+// answers[2] = prompt('сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+// let number = 4; 
+
+// console.log(-4/0);
+// console.log('string'*9);
+
+// const persone = `Alex`; 
+
+// const bool = true/false;
+
+// console.log(something); null
+
+// let und;
+// console.log(und); undefined 
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj.'name');
+
+      
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.png', {}, []];
+// console.log(arr[1]);
+
+
+
+
+// let number = 5; 
+// const leftBorderWidth = 1;   
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// var name = "Marina";
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+
+
